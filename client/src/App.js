c49af34d9de71b87108f1c65ee198a43ec9bff19
Farhan <farhanahmed2k177@gmail.com>
2023-06-24 16:54:35 +0500

    All crud components

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,68 @@ import AddRecipe from './AddRecipe';
 
 
 
-
 function App() {
 
+const [title, settitle] = useState("")
+const [description, setdescription] = useState("")
+const [ingredients, setingredients] = useState("")
+const [instructions, setinstructions] = useState("")
+const [newrecipe, setnewrecipe] = useState("")
+
+
+
+  const addRecipe = () => {
+
+    fetch("http://localhost:5000/addRecipe", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify({
+        title,
+        description,
+        ingredients,
+        instructions,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setnewrecipe(data);
+      });
+  };
+  const updateRecipe = (id) => {
+    fetch(`http://localhost:5000/updateRecipe/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify({
+        title,
+        description,
+        ingredients,
+        instructions,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setnewrecipe(data);
+      });
+  };
+  const deleteRecipe = (id) => {
+    fetch(`http://localhost:5000/deleteRecipe/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setnewrecipe(data);
+      });
+  };
+  
+  
 
 
   const [recipe, setrecipe] = useState("")
-  const [newrecipe, setnewrecipe] = useState("")
+
 useEffect(() => {
   fetch("http://localhost:5000/showRecipes")
   .then(res => res.json())
@@ -25,7 +80,6 @@ useEffect(() => {
   return (
    <>
 
-   <AddRecipe/>
     <div className="App">
       
       <h1>Recipes</h1>
@@ -44,8 +98,89 @@ useEffect(() => {
           }>View</button>
 
           </div>
-      ))}</div>
+      ))}
+      </div>
+
+      <div className="addRecipe">
+        <h2>Add Recipe</h2>
+        <input
+
+          type="text"
+          placeholder="title"
+          onChange={(e) => settitle(e.target.value)}
+        />
+        <input
+
+          type="text"
+          placeholder="description"
+          onChange={(e) => setdescription(e.target.value)}
+        />
+        <input
+        
+          
+          type="text"
+          placeholder="ingredients"
+          onChange={(e) => setingredients(e.target.value)}
+        />
+        <input
+
+          
+          type="text"
+          placeholder="instructions"
+          onChange={(e) => setinstructions(e.target.value)}
+        />
+        <button onClick={addRecipe}>Add</button>
+
     </div>
+    <div className="updateRecipe">
+          
+          <h2>Update Recipe</h2>
+          <input
+
+            type="text"
+            placeholder="title"
+            onChange={(e) => settitle(e.target.value)}
+          />
+          <input
+
+            type="text"
+            placeholder="description"
+            onChange={(e) => setdescription(e.target.value)}
+          />
+          <input
+
+          
+            type="text"
+            placeholder="ingredients"
+            onChange={(e) => setingredients(e.target.value)}
+          />
+          <input
+
+          type="text"
+          placeholder="instructions"
+          onChange={(e) => setinstructions(e.target.value)}
+        />
+        <button onClick={updateRecipe}>Update</button>
+        </div>
+
+        </div>
+        <div className="deleteRecipe">
+        <h2>Delete Recipe</h2>
+        <input
+          type="text"
+          placeholder="title"
+          onChange={(e) => settitle(e.target.value)}
+        />
+
+
+
+        <button onClick={deleteRecipe}>Delete</button>
+
+     </div>
+
+
+          
+    
 
     </> 
   );
